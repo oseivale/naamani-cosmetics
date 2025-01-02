5ad3ac6758f4f32055adf2b97293b72769ccfaa9
Val O <valerie.osei@gmail.com>
2025-01-02 18:49:52 -0500

    adding image host

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'example.com',
+        pathname: '/**', // Allow all paths
+      },
+      {
+        protocol: 'https',
+        hostname: 'venedorcitrus.myshopify.com',
+        pathname: '/cdn/shop/products/**', // Matches your product images
+      },
+    ],
+    domains: ['venedorcitrus.myshopify.com'], // Alternative configuration for simpler setups
+  },
 };
 
 module.exports = nextConfig;
--- src/app/icons/instagram.tsx
+++ src/app/icons/instagram.tsx
@@ -5,16 +5,16 @@ export const Instagram = () => (
     height="24"
     viewBox="0 0 24 24"
   >
-    <g fill="none" stroke="#ffffff" stroke-width="1.5">
+    <g fill="none" stroke="#ffffff" strokeWidth="1.5">
       <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         d="M12 16a4 4 0 1 0 0-8a4 4 0 0 0 0 8"
       />
       <path d="M3 16V8a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v8a5 5 0 0 1-5 5H8a5 5 0 0 1-5-5Z" />
       <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         d="m17.5 6.51l.01-.011"
       />
     </g>
