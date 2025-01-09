0238ea3756c5a08a5cef42dd2323c58adb0d60fe
Val O <valerie.osei@gmail.com>
2025-01-09 13:13:58 -0500

    small adjustments

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -28,7 +28,7 @@ export default function ProductsPage() {
     // <CartProvider>
     <PageBanner
       title={"Shop"}
-      backgroundImage="https://venedorcitrus.myshopify.com/cdn/shop/products/ce535e88-c69c-4e07-8cb3-7f90c3ca7a21_05.jpg?v=1604581404&width=220"
+      backgroundImage="https://images.unsplash.com/photo-1515377905703-c4788e51af15?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
       // backgroundImage="https://images.unsplash.com/photo-1515377905703-c4788e51af15?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     >
       <div style={{ padding: "2rem" }}>
