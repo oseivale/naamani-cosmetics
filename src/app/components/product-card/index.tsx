782b4b099dab028619771a31e4da8ddb59dfb149
User <user@Users-MacBook-Pro.local>
2025-12-05 23:06:23 -0500

    working on styling

--- src/app/components/product-card/index.tsx
+++ src/app/components/product-card/index.tsx
@@ -10,7 +10,7 @@ export default function ProductCard({ name, price, description, id, mainImage}: 
         <div>
           <div className={styles.productImg}>
             {/* <Image src="https://venedorcitrus.myshopify.com/cdn/shop/products/dbcada3-65f5-41ba-a914-23a44b09575b_01.jpg?v=1604582479&width=400" alt={'#'} height={100} width={100} /> */}
-            <Image src={mainImage} alt={'#'} height={100} width={100} />
+            <Image src={mainImage} alt={'#'} height={1000} width={1000} />
           </div>
           <h3>{`${name?.slice(0,25)}`}{name.length > 24 ? `...` : ''}</h3>
           <p className={styles.productDescription}>{`${description?.slice(0,40)}...`}</p>
